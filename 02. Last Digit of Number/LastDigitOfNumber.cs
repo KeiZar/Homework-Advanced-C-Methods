50bf1de8ce98042f07ca7927863424b2819cea4e
Keizar <matrixworlds@gmail.com>
2015-05-10 13:56:42 +0300

    Added Problem 2

---
+++ 02. Last Digit of Number/LastDigitOfNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+
+class LastDigitOfNumber
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetDigitAsWord(inputNumber));
+    }
+    static string GetDigitAsWord(int number)
+    {
+        string[] numberAsWord = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        int digitWord = number % 10;
+
+        return numberAsWord[digitWord];
+    }
+}
+
