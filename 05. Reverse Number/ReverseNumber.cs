cf4fc7d29d0e96831cb4075c89585b3571299619
Keizar <matrixworlds@gmail.com>
2015-05-10 15:02:31 +0300

    Added Problem 5

---
+++ 05. Reverse Number/ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        double input = double.Parse(Console.ReadLine());
+        double reversed = GetReversedNumber(input);
+        Console.WriteLine(reversed);
+    }
+
+    private static double GetReversedNumber(double notReversed)
+    {
+        string stringNumber = notReversed.ToString();
+        char[] reversed = stringNumber.ToCharArray();
+        Array.Reverse(reversed);
+        string reversedNumber = new string(reversed);
+        double newNumber = double.Parse(reversedNumber);
+        return newNumber;
+
+    }
+}
+
